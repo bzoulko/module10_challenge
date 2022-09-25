7ce50b9272986ae9a5b76a7ae6d297e8eef030cc
BrianZoulko <bzoulko@gmail.com>
2022-09-25 11:23:37 -0700

    Working on menu selection process.

--- src/teamProfileGenerator.js
+++ src/teamProfileGenerator.js
@@ -11,7 +11,7 @@
 // Required modules for this application.
 const inquirer =    require('inquirer');
 const fs =          require('fs');
-const { clear } =   require('console');
+const { clear, Console } =   require('console');
 const htmlPage =    require("../lib/HtmlTemplate.js");
 const jsdom =       require("jsdom"); // This library will allow JQuery.
   
@@ -49,7 +49,7 @@ const menuPrompt = () => {
     {
       type: "list",
       name: "menu",
-      message: "Add Option: ",
+      message: "Add aother member to my team: ",
       choices: ["engineer", "intern", "finished"],
     }
   ]);
@@ -122,49 +122,20 @@ function screenTitle() {
   console.log("***********************************************");
 }
 
-
 // Start CLI Process.
 function startCLI() {
+  var manager = null;
+  var employees = [];
+
   screenTitle();
   managerPrompt()
-    // Use writeFileSync method to use promises instead of a callback function
+        
     .then((answers) => {
       let name = answers.manager;
       let ID = answers.ID;
       let email = answers.email;
       let office = answers.office;
-
-      // Creating a window with a document
-      const dom = new jsdom.JSDOM(generateHTML(answers));
-        
-      // Importing the jquery and providing it
-      // with the window
-      const jquery = require("jquery")(dom.window);
-
-      // Add Manager's card to the HTML.      
-      let formContent = jquery(".form-content");
-      const mainSection = jquery("<section>");
-      const cardSection = jquery("<section class='card'>");
-      
-      const divTop = jquery("<div>");
-      const divTitleLine = jquery(`<p class="header-line1">`).text(name);
-      const divOccupation = jquery(`<p class="header-line2">`).text("Manager");
-      divTop.append(divTitleLine).append(divOccupation);
-
-      const divBottom = jquery("<div class='card-bottom'>");
-      const divID = jquery(`<div class='card-inside'>`).text("ID: " + ID);
-      const aEmail = jquery(`<a class="email-link">`).text(email);
-      const divEmail = jquery("<div class='card-inside'>").text("Email: ").append(aEmail);
-      const divType = jquery(`<div class='card-inside'>`).text(office);
-      divType.append(jquery(`<a>`));
-      divBottom.append(divID).append(divEmail).append(divType);
-
-      cardSection.append(divTop).append(divBottom);
-      mainSection.append(cardSection);
-      formContent.append(mainSection);
-
-      let htmlData = jquery("html")
-      fs.writeFileSync('index.html', "<!DOCTYPE html><html lang='en'>" + htmlData.html() + "</html>");
+      manager = new Manager(name, email, office);
 
     })
     .then(() => {
@@ -173,8 +144,14 @@ function startCLI() {
       menuPrompt()
         // Use writeFileSync method to use promises instead of a callback function
         .then((answers) => {
-          console.log("check file now...");
-          //fs.writeFileSync('index.html', generateHTML(answers))
+
+          console.log("answers.menu: " + answers.menu);
+          // let name = answers.manager;
+          // let ID = answers.ID;
+          // let email = answers.email;
+          // let office = answers.office;
+          // employees.push(new Employee(name, email, office));
+    
         })
         .then(() => console.log('Successfully wrote to index.html'))
         .catch((err) => console.error(err));
@@ -190,3 +167,41 @@ startCLI();
 
 // // Include functions from additional javascript files.
 // import {getHTMLTemplate} from '../lib/HtmlTemplate.js';
+
+function buildIndexHTML (manager, employeeList) {
+
+  // Creating a window with a document
+  var dom = new jsdom.JSDOM(generateHTML(answers));
+
+  // Importing the jquery and providing it
+  // with the window
+  var jquery = require("jquery")(dom.window);
+  var manager = null;
+
+
+  // Add Manager's card to the HTML.
+  let formContent = jquery(".form-content");
+  const mainSection = jquery("<section>");
+  const cardSection = jquery("<section class='card'>");
+  
+  const divTop = jquery("<div>");
+  const divTitleLine = jquery(`<p class="header-line1">`).text(name);
+  const divOccupation = jquery(`<p class="header-line2">`).text("Manager");
+  divTop.append(divTitleLine).append(divOccupation);
+
+  const divBottom = jquery("<div class='card-bottom'>");
+  const divID = jquery(`<div class='card-inside'>`).text("ID: " + ID);
+  const aEmail = jquery(`<a class="email-link">`).text(email);
+  const divEmail = jquery("<div class='card-inside'>").text("Email: ").append(aEmail);
+  const divType = jquery(`<div class='card-inside'>`).text(office);
+  divType.append(jquery(`<a>`));
+  divBottom.append(divID).append(divEmail).append(divType);
+
+  cardSection.append(divTop).append(divBottom);
+  mainSection.append(cardSection);
+  formContent.append(mainSection);
+
+  let htmlData = jquery("html")
+  fs.writeFileSync('index.html', "<!DOCTYPE html><html lang='en'>" + htmlData.html() + "</html>");
+  
+}
